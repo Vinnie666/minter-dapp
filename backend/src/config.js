b460493f9bad2c85ef2ea72f26f686f4836f926a
Anasazi007 <marc.r.wilson.1.9.6.9@gmail.com>
2022-05-09 15:37:41 -0400

    updates

--- backend/src/config.js
+++ backend/src/config.js
@@ -49,7 +49,7 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
+const CONTRACT_NAME = 'YOUR COLLECTION NAME';
 const CONTRACT_SYMBOL = 'CP';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
